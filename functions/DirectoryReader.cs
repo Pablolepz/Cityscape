80df79979d453dd656257193140a0eab6c0fc697
Rene Barahona <rene.barahona42@gmail.com>
2019-10-26 01:39:38 -0700

    Removed exe file. Added support for Volumes Implemented DriveInfo, DirectoryInfo, and FileInfo classes. Added a check for FileAttributes (hidden, system).

--- functions/DirectoryReader.cs
+++ functions/DirectoryReader.cs
@@ -6,36 +6,75 @@ using System.IO;
 
 namespace DirectoryReader{
     class DirectoryReader{
-        enum Type {FILE, FOLDER};
         static void Main(){
-            String path = @"C:\";
-            Console.WriteLine("Listing: " + path);
+            Console.WriteLine("----------DRIVES----------");
+            DriveInfo[] drives = GetVolumes();
+            WriteList(drives);
+            String path = drives[0].RootDirectory.ToString();
             
-            String[] dirs = GetDirectories(path);
-            String[] files = GetFiles(path);
-            Console.WriteLine("-----------DIRECTORIES----------");
-            WriteList(dirs, Type.FOLDER);
-            Console.WriteLine("-----------FILES----------------");
-            WriteList(files, Type.FILE);
+            Console.WriteLine("----------(C:)------------");
+            Console.WriteLine("Listing: " + path);
+            DirectoryInfo[] dirs = GetDirectories(path);
+            FileInfo[] files = GetFiles(path);
+            Console.WriteLine("----------DIRECTORIES-----");
+            WriteList(dirs);
+            Console.WriteLine("----------FILES-----------");
+            WriteList(files);
+
+            
         }
-        static String[] GetDirectories(String path){
-            return Enumerable.ToArray(
-                Directory.EnumerateDirectories(path)
-            );
+        static DriveInfo[] GetVolumes(){
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            return drives;
         }
-        static String[] GetFiles(String path){
-            return Enumerable.ToArray(
-                Directory.EnumerateFiles(path)
-            );
-        }
-        static void WriteList(String[] list, Type type){
-            char c = (
-                type == Type.FOLDER ? '\\' : '\0'
-            );
-            foreach(String s in list){
-                int trimPoint = s.LastIndexOf(@"\")+1;
-                Console.WriteLine("    " + s.Substring(trimPoint) + c);
+        static DirectoryInfo[] GetDirectories(String path){
+            var list = Directory.EnumerateDirectories(path);
+            IEnumerator e = list.GetEnumerator();
+            DirectoryInfo[] directories = new DirectoryInfo[list.Count()];
+            for(int i = 0; e.MoveNext(); i++){
+                directories[i] = new DirectoryInfo(e.Current.ToString());
             }
+            return directories;
+        }
+        static FileInfo[] GetFiles(String path){
+            var list = Directory.EnumerateFiles(path);
+            IEnumerator e = list.GetEnumerator();
+            FileInfo[] files = new FileInfo[list.Count()];
+            for(int i = 0; e.MoveNext(); i++){
+                files[i] = new FileInfo(e.Current.ToString());
+            }
+            return files;
+        }
+        static void WriteList(DirectoryInfo[] list){
+            foreach(DirectoryInfo dir in list){
+                if(!(IsSystem(dir.Attributes) || IsHidden(dir.Attributes)))
+                    Console.WriteLine("    " + dir.Name + @"\");
+            }
+        }
+        static void WriteList(FileInfo[] list){
+            foreach(FileInfo file in list){
+                if(!IsSystem(file.Attributes))
+                    Console.WriteLine("    " + file.Name);
+            }
+        }
+        static void WriteList(DriveInfo[] list){
+            foreach(DriveInfo d in list){
+                Console.WriteLine(
+                    d.ToString() + "    ("
+                        + PercentageUsed(d).ToString("###.##")
+                        + "% used)    "
+                        + d.DriveType
+                    );
+            }
+        }
+        static bool IsHidden(FileAttributes attributes){
+            return attributes.HasFlag(FileAttributes.Hidden);
+        }
+        static bool IsSystem(FileAttributes attributes){
+            return attributes.HasFlag(FileAttributes.System);
+        }
+        static double PercentageUsed(DriveInfo drive){
+            return (((double)drive.AvailableFreeSpace/drive.TotalSize)*100);
         }
     }
 }
