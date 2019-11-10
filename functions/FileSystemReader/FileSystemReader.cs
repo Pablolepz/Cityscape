919833616843ce83faeba2105387aa0ec2676ee7
Rene Barahona <rene.barahona42@gmail.com>
2019-11-10 02:12:20 -0800

    Wrapper class to access items in the file system.
    Updated README to include detailed documentation and TODO.
    Added class FileSystemReader to provide an easy way to get mounted volumes and contents of a directory.
    Added static class PathReader to mediate between PathItem.
    Added a class PathItem to represent files, directories, and volumes.
    Deprecated and removed other prototype classes.

---
+++ functions/FileSystemReader/FileSystemReader.cs
@@ -0,0 +1,70 @@
+using System.IO;
+using System.Collections.Generic;
+
+namespace FileSystemReader
+{
+
+    class FileSystemReader
+    {
+        private List<PathItem> _files = new List<PathItem>();
+        private List<PathItem> _directories = new List<PathItem>();
+        private List<PathItem> _volumes = new List<PathItem>();
+
+        public int FileCount { get => _files.Count; }
+        public int DirectoryCount { get => _directories.Count; }
+        public int VolumeCount { get => _volumes.Count; }
+        public List<PathItem> Files { get => _files; }
+        public List<PathItem> Directories { get => _directories; }
+        public List<PathItem> Volumes { get => _volumes; }
+        public List<PathItem> FilesAndDirectories {
+            get => JoinFilesDirectories();
+        }
+
+        public FileSystemReader() { }
+
+        private List<PathItem> FetchFiles(string path)
+        {
+            _files = PathReader.GetFiles(path);
+            return _files;
+        }
+        private List<PathItem> FetchDirectories(string path)
+        {
+            _directories = PathReader.GetDirectories(path);
+            return _directories;
+        }
+        private List<PathItem> FetchVolumes(string path)
+        {
+            _volumes = PathReader.GetVolumes(path);
+            return _volumes;
+        }
+        private List<PathItem> JoinFilesDirectories()
+        {
+            List<PathItem> allItems = new List<PathItem>();
+            allItems.AddRange(_files);
+            allItems.AddRange(_directories);
+            return allItems;
+        }
+        public void FetchAll(string path)
+        {
+            FetchFiles(path);
+            FetchDirectories(path);
+            FetchVolumes(path);
+        }
+        public void FetchFromPath(string path)
+        {
+            FetchFiles(path);
+            FetchDirectories(path);
+        }
+        public override string ToString()
+        {
+            return (
+                    "[" + _files.Count +"] files, [" +
+                    _directories.Count + "] directories, [" +
+                    _volumes.Count + "] volumes."
+                );
+        }
+        // public List<PathItem> Sort(SortRule rule){
+        //     return null;
+        // }
+    }
+}
---
+++ functions/FileSystemReader/PathReader.cs
@@ -0,0 +1,49 @@
+using System.IO;
+using System.Collections.Generic;
+
+namespace FileSystemReader
+{
+    internal static class PathReader
+    {
+        internal static List<PathItem> GetFiles(string path){
+            string[] files = GetFileStrings(path);
+            return GetPathItemList(files, PathItem.Type.File);
+        }
+        internal static List<PathItem> GetDirectories(string path){
+            string[] directories = GetDirectoryStrings(path);
+            return GetPathItemList(
+                directories, PathItem.Type.Directory
+            );
+        }
+        internal static List<PathItem> GetVolumes(string path){
+            string[] volumes = GetVolumeStrings();
+            return GetPathItemList(volumes, PathItem.Type.Volume);
+        }
+        private static List<PathItem> GetPathItemList(
+            string[] paths, PathItem.Type type
+        ){
+            List<PathItem> pathItems = new List<PathItem>();
+            foreach(string path in paths){
+                pathItems.Add(new PathItem(path, type));
+            }
+            return pathItems;
+        }
+        private static string[] GetFileStrings(string path) =>
+            Directory.GetFiles(path);
+        private static string[] GetDirectoryStrings(string path) =>
+            Directory.GetDirectories(path);
+        private static string[] GetVolumeStrings(){
+            DriveInfo[] driveInfo = GetVolumeInfo();
+            return GetDriveStrings(driveInfo);
+        }
+        private static DriveInfo[] GetVolumeInfo() =>
+            DriveInfo.GetDrives();
+        private static string[] GetDriveStrings(DriveInfo[] driveInfo){
+            string[] volumes = new string[driveInfo.Length];
+            for(int i = 0; i < driveInfo.Length; i++){
+                volumes[i] = driveInfo[i].ToString();
+            }
+            return volumes;
+        }
+    }
+}
